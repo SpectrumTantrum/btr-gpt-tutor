e664c7f04f749b26646c4ecb9a1e6fbd5ffba2b4
SpectrumTantrum <119471486+SpectrumTantrum@users.noreply.github.com>
2026-04-13 18:28:10 +0800

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "btr-gpt-tutor",
@@ -14,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
